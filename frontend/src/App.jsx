353fcc274a178d4f259976f5319a963c2c88713d
Keshav <keshavdeepak2002@gmail.com>
2025-10-29 19:19:01 +0530

    bug fix - backend was only getting the new question, not the full conversation history

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,11 +23,10 @@ export default function App() {
 			content : question,
 		};
 
-		//* append to messages
-		setMessages((messages) => [...messages, question_message]);
+		setMessages((messages) => [...messages, question_message])
 
 		//* send to backend and receive an answer back
-		sendToBackend(question).then(response => {
+		sendToBackend([...messages, question_message]).then(response => {
 			//* transform the response into a message
 			let response_message = {
 				id : messages_length + 2,
@@ -35,7 +34,7 @@ export default function App() {
 				content : response.messages[response.messages.length-1].content
 			};
 
-			//* append to messages
+			//* append response to messages
 			setMessages((messages) => [...messages, response_message]);
 		})
 	}
@@ -64,33 +63,3 @@ const sendToBackend = async (messages) => {
 
 	return full_response;
 }
-
-
-/*
-const [count, setCount] = useState(0)
-
-  return (
-	<>
-	  <div>
-		<a href="https://vite.dev" target="_blank">
-		  <img src={viteLogo} className="logo" alt="Vite logo" />
-		</a>
-		<a href="https://react.dev" target="_blank">
-		  <img src={reactLogo} className="logo react" alt="React logo" />
-		</a>
-	  </div>
-	  <h1>Vite + React</h1>
-	  <div className="card">
-		<button onClick={() => setCount((count) => count + 1)}>
-		  count is {count}
-		</button>
-		<p>
-		  Edit <code>src/App.jsx</code> and save to test HMR
-		</p>
-	  </div>
-	  <p className="read-the-docs">
-		Click on the Vite and React logos to learn more
-	  </p>
-	</>
-  )
-*/
