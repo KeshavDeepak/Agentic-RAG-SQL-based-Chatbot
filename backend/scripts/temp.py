01b9d7cb3d1fb5b1d3bc876d0d773ba7ac29f038
KeshavDeepak <keshavdeepak2002@gmail.com>
2025-11-04 14:23:07 +0530

    imported tailwind

--- backend/scripts/temp.py
+++ backend/scripts/temp.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("backend/data/adventureworks_new.sqlite")
+conn = sqlite3.connect("backend/data/adventureworks.sqlite")
 cursor = conn.cursor()
 
 cursor.execute("pragma table_info('Sales.Store')")
--- frontend/src/components/Conversation.jsx
+++ frontend/src/components/Conversation.jsx
@@ -10,9 +10,15 @@ function Conversation({ messages }) {
         )
     }
 
-    return <>
+    return (
+    <>
+        {/* <div className="p-4 mb-4 bg-gradient-to-r from-indigo-500 to-purple-600 text-white font-bold rounded-xl shadow-lg">
+            Tailwind is working
+        </div> */}
+        
         {conversation_log}
     </>
+    )
 }
 
 export default Conversation
